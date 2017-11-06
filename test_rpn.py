f1c73f123cd0b4b3e18cc155ea40d92c802c2a15
vamsi <vimmada@umich.edu>
2017-11-06 18:09:23 -0500

    even more decrease

--- test_rpn.py
+++ test_rpn.py
@@ -5,14 +5,14 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
-	def test_subtract(self):
-		result = rpn.calculate('5 3 -')
-		self.assertEqual(2, result)
-	def test_multiply(self):
-		result = rpn.calculate('2 3 *')
-		self.assertEqual(6, result)
-		result = rpn.calculate('3 5 *')
-		self.assertEqual(15, result)
+	# def test_subtract(self):
+	# 	result = rpn.calculate('5 3 -')
+	# 	self.assertEqual(2, result)
+	# def test_multiply(self):
+	# 	result = rpn.calculate('2 3 *')
+	# 	self.assertEqual(6, result)
+	# 	result = rpn.calculate('3 5 *')
+	# 	self.assertEqual(15, result)
 	# def test_divide(self):
 	# 	result = rpn.calculate('2 10 /')
 	# 	self.assertEqual(0.2, result)
